080038234a4f3edb74140322333db782b41a7cf5
sazzadrupak <sazzad@oployeelabs.com>
2024-09-25 17:44:00 +0300

    Interaction testing

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,5 +1,6 @@
 const lib = require('../lib');
 const db = require('../db');
+const mail = require('../mail');
 
 describe('absolute', () => {
   it('should return a positive num if input is positive', () => {
@@ -69,5 +70,21 @@ describe('applyDiscount', () => {
     const order = { customerId: 1, totalPrice: 10 };
     lib.applyDiscount(order);
     expect(order.totalPrice).toBe(9);
+  });
+});
+
+describe('notifyCustomer', () => {
+  it('should send an email to the customer', () => {
+    db.getCustomerSync = (customerId) => {
+      return { email: 'a' };
+    };
+
+    let mailSent = false;
+    mail.send = (email, message) => {
+      mailSent = true;
+    };
+    lib.notifyCustomer({ customerId: 1 });
+
+    expect(mailSent).toBe(true);
   });
 });
